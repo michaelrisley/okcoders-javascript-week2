c2c4cf30eeb2cfb2fc581e10718070be4df68973
Michael A. Risley <michael.risley@gmail.com>
2018-03-10 10:29:58 -0600

    DRY version

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-console.log("----------------------")
+var beers = 99;
 
-var bottles = 99
+function printVerse(num) {
 
-while (bottles != 0) 
-	{
-	console.log(bottles +" bottles of beer on the wall");
-	console.log(bottles +" bottles of beer");
-	console.log("you take one down, you pass it around");
-	bottles = bottles - 1;
-	console.log(bottles +" bottles of beer on the wall");
-	console.log("");
+	var name = "bottles";
+	if (num === 1) {name = "bottle";}
+
+	console.log(num + " " + name + " of beer on the wall");
+	console.log(num + " " + name + " bottles of beer")
+	console.log("You take one down, you pass it around.");
+	num--;
+	console.log(num + " bottles of beer on the wall\n")
+	return num;
+}
+
+while (beers > 0) {
+	beers = printVerse(beers);
 	}
 
 // You can also use "beers--" which is exactly the same as "bottles = bottles - 1". You can also use beer++ for the reverse
+// extra comment because I forgot to add a message to my commit
