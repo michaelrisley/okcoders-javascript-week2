a710ca58f92bc8cacbf9e46dce26b7df73e98391
Michael A. Risley <michael.risley@gmail.com>
2018-03-10 10:43:28 -0600

    DRYest Version

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var beers = 99;
-
 function printVerse(num) {
 
 	var name = "bottles";
@@ -10,12 +8,8 @@ function printVerse(num) {
 	console.log("You take one down, you pass it around.");
 	num--;
 	console.log(num + " bottles of beer on the wall\n")
-	return num;
 }
 
-while (beers > 0) {
-	beers = printVerse(beers);
-	}
-
-// You can also use "beers--" which is exactly the same as "bottles = bottles - 1". You can also use beer++ for the reverse
-// extra comment because I forgot to add a message to my commit
+for (var x = 99; x > 0; x--) {
+	printVerse(x);
+}
